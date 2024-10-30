2f3a931abd72a803d550bafe23249c3629614a26
Asadullah Pranto <mubinkhan819@gmail.com>
2024-10-30 10:16:04 +0600

    Initial Commit

---
+++ TestRepoTests/TestRepoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoTests.swift
+//  TestRepoTests
+//
+//  Created by Mubin Khan on 10/30/24.
+//
+
+import Testing
+@testable import TestRepo
+
+struct TestRepoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
